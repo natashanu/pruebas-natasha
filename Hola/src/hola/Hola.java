8ef849541c6f6c96dd74ee4049047786794d8b15
Natasha Nuñez <natashanu6@gmail.com>
2022-05-19 22:17:51 +0200

    Codigo fuente inicial

---
+++ Hola/src/hola/Hola.java
@@ -0,0 +1,20 @@
+/*
+ *
+ *
+ */
+package hola;
+
+/**
+ *
+ * @author Natasha
+ */
+public class Hola {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+  }
+  
+}
